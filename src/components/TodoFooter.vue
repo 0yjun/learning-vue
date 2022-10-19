ea3a12ad086b19eb9d8b822b71c34b2a806f2040
jun <an0yjun@naver.com>
2022-10-19 10:25:10 +0900

    first

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo">
+      clear all
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  methods : {
+    clearTodo : function(){
+      localStorage.clear();
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .clearAllContainer {
+    width: 8.5rem;
+    height : 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+  }
+  .clearAllBtn{
+    display : block;
+    color : #E20303;
+  }
+</style>
+  
