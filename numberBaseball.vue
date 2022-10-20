2a090c7bcd2265d0de5d11b67e56d86b0452ffa7
jun <an0yjun@naver.com>
2022-10-20 23:56:50 +0900

    vue game

---
+++ numberBaseball.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ result }}</div>
+  <form v-on:submit="onSubmitForm"></form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      result: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style></style>
